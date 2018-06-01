626ccfe8d2670061eb947ffa887a3febc7281069
Sebastian Koch <sebastian@0py.de>
2018-06-01 19:58:58 +0200

    Conditional compilation of Unity Log

--- UnityEngine.cs
+++ UnityEngine.cs
@@ -1,5 +1,8 @@
 using System;
 
+
+// hijacking unity log when building outside of unity.
+#if !(UNITY_EDITOR || UNITY_EDITOR_WIN || UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX || UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX || UNITY_STANDALONE || UNITY_WII || UNITY_IOS || UNITY_IPHONE || UNITY_ANDROID || UNITY_PS4 || UNITY_XBOXONE || UNITY_TIZEN || UNITY_TVOS || UNITY_WSA || UNITY_WSA_10_0 || UNITY_WINRT || UNITY_WINRT_10_0 || UNITY_WEBGL || UNITY_FACEBOOK || UNITY_ADS || UNITY_ANALYTICS || UNITY_ASSERTIONS)
 namespace UnityEngine {
     class Debug {
         public static void Log(string s) {
@@ -7,3 +10,4 @@ namespace UnityEngine {
         }
     }
 }
+#endif
