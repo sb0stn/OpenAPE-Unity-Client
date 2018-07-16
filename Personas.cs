6b39b081e3e2d433e2b4ec9463501463ea44d71f
Sebastian Koch <sebastian@0py.de>
2018-07-16 16:02:03 +0200

    Adds Personas class handling parsing of an XML file

---
+++ Personas.cs
@@ -0,0 +1,58 @@
+﻿using System.Xml;
+using System.Collections.Generic;
+
+namespace OpenAPE
+{
+    public class Personas
+    {
+        private static List<Persona> personas;
+
+
+        public Personas(string xml)
+        {         
+            var doc = new XmlDocument();
+            doc.LoadXml(xml);
+
+            var nodes = doc.DocumentElement.SelectNodes("/personas/persona");
+
+            personas = new List<Persona>(nodes.Count);
+            
+            foreach (XmlNode node in nodes)
+            {                
+                var persona = new Persona(
+                    node.SelectSingleNode("name").InnerText,
+                    node.SelectSingleNode("password").InnerText,
+                    node.SelectSingleNode("id").InnerText
+                ); 
+
+                personas.Add(persona);
+            }
+        }
+
+
+        public Persona Get(int idx)
+        {
+            return personas[idx];
+        }
+        
+        public List<Persona> GetAll()
+        {
+            return personas;
+        }
+    }
+    
+    
+    public class Persona
+    {
+        public readonly string Id;
+        public readonly string Password;
+        public readonly string Username;
+
+        internal Persona(string username, string password, string id)
+        {
+            Username = username;
+            Password = password;
+            Id = id;
+        }
+    }
+}
